77a91b7d6ede3ab812f41176d64e8f1f2327d178
Reginald Osano <rgo02439@gmail.com>
2024-05-08 08:29:59 +0800

    initial push

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+  
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
